7c586ebeef4f26a4ac115e4c4b716c493de1e6ee
leolinton <leonardo-l-oliveira@openlabs.com.br>
2025-09-12 19:06:07 -0300

    Atualização Final 12/09/25

--- assets/js/contato.js
+++ assets/js/contato.js
@@ -6,7 +6,7 @@ document.getElementById('whatsappForm').addEventListener('submit', function (e) 
     const email = document.getElementById('email').value.trim();
     const mensagem = document.getElementById('message').value.trim();
 
-    const numeroDestino = '5521976590294'; // Substitua pelo número de destino no formato internacional
+    const numeroDestino = '559236425881'; // Substitua pelo número de destino no formato internacional
     const texto = `Olá, meu nome é ${nome} \nTelefone: ${telefone}.\nEmail: ${email} \nAssunto: ${mensagem}`;
     const url = `https://wa.me/${numeroDestino}?text=${encodeURIComponent(texto)}`;
 
